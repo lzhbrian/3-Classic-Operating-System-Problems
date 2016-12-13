d5688219f86bce71fa428a647ac878a75c3c3274
lzhbrian <lzhbrian@gmail.com>
2016-12-13 11:42:00 +0800

    Finished 2nd proj

--- 2nd/generate_1m_numbers.py
+++ 2nd/generate_1m_numbers.py
@@ -8,7 +8,7 @@ max_x = 60000
 
 count = 0
 for x in xrange(0,length):
-	print >> fp, int(random.uniform(min_x, max_x))
+	print >> fp, (random.uniform(min_x, max_x))
 	count += 1
 
 print "Suc. print ", count, " numbers"
--- 2nd/quick_sort_multithread.cpp
+++ 2nd/quick_sort_multithread.cpp
@@ -20,31 +20,98 @@
 using namespace std;
 
 # define numbers_length 1000000
-int data[numbers_length];
+# define Datatype double
+
+Datatype data[numbers_length];
 int n = 0;
 long long open_time = 0;	// 程序开始时间
 
 // Get current time, return: ms
 long long getSystemTime() {
-    struct timeb t;
-    ftime(&t);
-    return 1000 * t.time + t.millitm;
+	struct timeb t;
+	ftime(&t);
+	return 1000 * t.time + t.millitm;
+}
+
+// 传给 Quick_sort 的参数结构体
+struct para
+{   
+	int start_index;  
+	int end_index;   
+};
+// exchange
+void exch(Datatype* a, Datatype* b) {
+	Datatype temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int partition(Datatype* a, int low, int up)  
+{  
+    Datatype pivot = a[up];  
+    int i = low-1;  
+    for (int j = low; j < up; j++)  
+    {  
+        if(a[j] <= pivot)  
+        {  
+            i++;  
+            exch(&a[i], &a[j]);  
+        }  
+    }  
+    exch(&a[i+1], &a[up]);  
+    return i+1;  
+}
+
+// 小于 1000 时调用的 quicksort
+void real_quick_sort(Datatype* a, int left, int right) {
+	if (right <= left) return;
+	int pos = partition(a,left,right);
+	real_quick_sort(a,left,pos-1);
+	real_quick_sort(a,pos+1,right);
 }
 
 
+
+int q_sort_call_time = 0;
+
 // quick sort unit
-void *Quick_sort(void* start_index, void* end_index) {
+void *Quick_sort(void* para_s) {
+
+	// get received arg
+	struct para *recv_para;  
+    recv_para = (struct para *)para_s;  
+    int start_index = (*recv_para).start_index;  
+    int end_index = (*recv_para).end_index;  
+
+	// length of data this thread can control
+	int data_length = end_index - start_index + 1;
+	if (data_length <= 1000) {
+		real_quick_sort(data, start_index, end_index);
+		return 0;
+	}
+
+	int *pos = new int(1);
+	*pos = partition(data, start_index, end_index);
 
 	pthread_t front_thread;
 	pthread_t back_thread;
 
+	struct para para_front;
+	struct para para_back;
+	
+	para_front.start_index = start_index;  
+	para_front.end_index = *pos-1;
+
+	para_back.start_index = *pos+1;  
+	para_back.end_index = end_index;
+
 	// Create front,back threads
-	if(pthread_create(&front_thread, NULL, Quick_sort, &tid_s[i])) {  
-		cout << "\n ERROR creating first main thread!" << endl;  
+	if(pthread_create(&front_thread, NULL, Quick_sort, &(para_front))) {  
+		cout << "\n ERROR creating front thread!" << endl;  
 		exit(1);
 	}
-	if(pthread_create(&back_thread, NULL, Quick_sort, &tid_s[i])) {  
-		cout << "\n ERROR creating first main thread!" << endl;  
+	if(pthread_create(&back_thread, NULL, Quick_sort, &(para_back))) {  
+		cout << "\n ERROR creating back thread!" << endl;  
 		exit(1);
 	}
 
@@ -58,7 +125,9 @@ void *Quick_sort(void* start_index, void* end_index) {
 		exit(1);
 	}
 
+	return 0;
 }
+
 
 
 
@@ -80,13 +149,16 @@ int main(int argc, char * argv[])
 	cout << getSystemTime() - open_time << " ms used to read " << numbers_length << " numbers..." << endl;
 
 	// test for read numbers
-	// cout << data[100] << ", " << data[200] << ", " << data[numbers_length-1] 
-			<< ", " << data[numbers_length] << endl;
+	// cout << data[100] << ", " << data[200] << ", " << data[numbers_length-1] << ", " << data[numbers_length] << endl;
 
 	pthread_t main_thread;
 
+	struct para pata_main;  
+	pata_main.start_index = 0;  
+	pata_main.end_index = numbers_length-1;
+
 	// Create main thread
-	if(pthread_create(&main_thread, NULL, Quick_sort, &tid_s[i])) {  
+	if(pthread_create( &main_thread, NULL, Quick_sort, &(pata_main) )) {  
 		cout << "\n ERROR creating first main thread!" << endl;  
 		exit(1);
 	}
@@ -98,6 +170,25 @@ int main(int argc, char * argv[])
 	}
 	
 
+	// Check result
+	cout << "First 10 numbers: \n";
+	for (int i = 0; i < 10; ++i) {
+		cout << data[i] << endl;
+	}
+	cout << endl;
+	cout << "Last 10 numbers: \n";
+	for (int i = numbers_length-10; i < numbers_length; ++i) {
+		cout << data[i] << endl;
+	}
+
+	// indicator of runtime
+	cout << getSystemTime() - open_time << " ms used to finish the whole program! " << endl;
+
+	// write sorted result to file
+	ofstream fout("./sorted_Numbers.txt");
+	for (int i = 0; i < numbers_length; ++i) {
+		fout << data[i] << endl;
+	}
 	return 0;
 }
 
