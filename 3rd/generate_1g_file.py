bf9dfc416481bdde8ea9ec168d01fb7c81b65d87
lzhbrian <lzhbrian@gmail.com>
2016-12-13 15:31:08 +0800

    finish 3rd

---
+++ 3rd/generate_1g_file.py
@@ -0,0 +1,18 @@
+# -*- encoding=utf8 -*-
+fp = open('./data/one_gig_file.txt','w')
+
+data_length = 1000000
+
+from random import Random
+def random_str(randomlength=8):
+    str = ''
+    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
+    length = len(chars) - 1
+    random = Random()
+    for i in range(randomlength):
+        str+=chars[random.randint(0, length)]
+    return str
+
+a = random_str(data_length)
+
+print >> fp, a
---
+++ 3rd/memmap_method.py
@@ -0,0 +1,34 @@
+
+# Brian Lin,Tzu-Heng's Work
+# Reverse a big char file by memmap in python.numpy
+
+# Mailto: lzhbrian@gmail.com
+# Github: github.com/lzhbrian
+# Linkedin: linkedin/in/lzhbrian
+
+
+import sys
+import numpy as np
+
+# open source fike
+f = np.memmap('./data/one_gig_file.txt', mode='r')
+# f = np.memmap('./data/small_file.txt', mode='r')
+
+# calc length
+data_length = len(f)
+
+# open write file
+fp = np.memmap('./data/reversed_file.txt', shape=(data_length-1), mode='w+')
+
+# from 0 to data_length-2, does not include data_length-2
+reversed_arr = f[0:data_length-1] 
+
+# reverse!
+reversed_arr = reversed_arr[::-1]
+
+# copy to fp
+fp[:] = reversed_arr[:]
+
+# close file
+fp.flush()
+
